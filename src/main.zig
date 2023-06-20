57086988cf7a79ab3edd1a04d67134d5d8669001
Jonathan <sandalmoth@pm.me>
2023-06-20 23:54:53 +0200

    add a test for join/meet

--- src/main.zig
+++ src/main.zig
@@ -293,194 +293,50 @@ pub const CL201 = struct {
     }
 };
 
-pub const CL201MV = struct {
-    pub const MV = struct {
-        s: f32 = 0,
-        e0: f32 = 0,
-        e1: f32 = 0,
-        e2: f32 = 0,
-        e01: f32 = 0,
-        e20: f32 = 0,
-        e12: f32 = 0,
-        e012: f32 = 0,
+test "meet and join" {
+    // if we join three points into two lines
+    // then those lines should always meet in the shared point
+    const ga = CL201;
 
-        pub fn fromCart(x: f32, y: f32) MV {
-            return MV{ .e20 = x, .e01 = y, .e12 = 1 };
-        } // cartesian x/y coordinates
-
-        pub fn fromEq(a: f32, b: f32, c: f32) MV {
-            return MV{ .e1 = a, .e2 = b, .e0 = c };
-        } // line equation ax + by + c = 0
+    const points = [_]ga.Point{
+        ga.Point.fromCart(1, 2),
+        ga.Point.fromCart(3, -4),
+        ga.Point.fromCart(-5, 6),
+        ga.Point.fromCart(-7, -8),
     };
 
-    pub fn dual(mv: MV) MV {
-        return MV{
-            .s = mv.e012,
-            .e0 = mv.e12,
-            .e1 = mv.e20,
-            .e2 = mv.e01,
-            .e01 = mv.e2,
-            .e20 = mv.e1,
-            .e12 = mv.e0,
-            .e012 = mv.s,
-        };
+    // std.debug.print("\n", .{});
+    for (points) |p1| {
+        for (points) |p2| {
+            for (points) |p3| {
+                if (std.meta.eql(p1, p2) or std.meta.eql(p1, p3) or std.meta.eql(p2, p3)) {
+                    continue;
+                }
+                const l1 = ga.join(p1, p2);
+                const l2 = ga.join(p2, p3);
+                var p = ga.meet(l1, l2);
+                p.toCart();
+                // std.debug.print("{}\n", .{p2});
+                // std.debug.print("{}\n", .{p});
+                try std.testing.expectApproxEqAbs(p2.e20, p.e20, 1e-6);
+                try std.testing.expectApproxEqAbs(p2.e01, p.e01, 1e-6);
+                try std.testing.expectApproxEqAbs(p2.e12, p.e12, 1e-6);
+            }
+        }
     }
-
-    pub fn rev(mv: MV) MV {
-        var mv2 = mv;
-        mv2.e01 = -mv2.e01;
-        mv2.e20 = -mv2.e20;
-        mv2.e12 = -mv2.e12;
-        mv2.e012 = -mv2.e012;
-        return mv2;
-    }
-
-    pub fn conj(mv: MV) MV {
-        var mv2 = mv;
-        mv2.e0 = -mv.e0;
-        mv2.e1 = -mv.e1;
-        mv2.e2 = -mv.e2;
-        mv2.e01 = -mv2.e01;
-        mv2.e20 = -mv2.e20;
-        mv2.e12 = -mv2.e12;
-        return mv2;
-    }
-
-    pub fn normalize(mv: MV) MV {
-        const n = 1.0 / @sqrt(@fabs(mul(mv, conj(mv)).s));
-        return smul(n, mv);
-    }
-
-    /// geometric product
-    pub fn mul(a: MV, b: MV) MV {
-        return MV{
-            .s = a.s * b.s + a.e1 * b.e1 + a.e2 * b.e2 - a.e12 * b.e12,
-            .e0 = b.e0 * a.s + b.s * a.e0 - b.e01 * a.e1 + b.e20 * a.e2 +
-                b.e1 * a.e01 - b.e2 * a.e20 - b.e012 * a.e12 - b.e12 * a.e012,
-            .e1 = b.e1 * a.s + b.s * a.e1 - b.e12 * a.e2 + b.e2 * a.e12,
-            .e2 = b.e2 * a.s + b.e12 * a.e1 + b.s * a.e2 - b.e1 * a.e12,
-            .e01 = b.e01 * a.s + b.e1 * a.e0 - b.e0 * a.e1 + b.e012 * a.e2 +
-                b.s * a.e01 + b.e12 * a.e20 - b.e20 * a.e12 + b.e2 * a.e012,
-            .e20 = b.e20 * a.s - b.e2 * a.e0 + b.e012 * a.e1 + b.e0 * a.e2 -
-                b.e12 * a.e01 + b.s * a.e20 + b.e01 * a.e12 + b.e1 * a.e012,
-            .e12 = b.e12 * a.s + b.e2 * a.e1 - b.e1 * a.e2 + b.s * a.e12,
-            .e012 = b.e012 * a.s + b.e12 * a.e0 + b.e20 * a.e1 + b.e01 * a.e2 +
-                b.e2 * a.e01 + b.e1 * a.e20 + b.e0 * a.e12 + b.s * a.e012,
-        };
-    }
-
-    /// outer product
-    pub fn meet(a: MV, b: MV) MV {
-        return MV{
-            .s = b.s * a.s,
-            .e0 = b.e0 * a.s + b.s * a.e0,
-            .e1 = b.e1 * a.s + b.s * a.e1,
-            .e2 = b.e2 * a.s + b.s * a.e2,
-            .e01 = b.e01 * a.s + b.e1 * a.e0 - b.e0 * a.e1 + b.s * a.e01,
-            .e20 = b.e20 * a.s - b.e2 * a.e0 + b.e0 * a.e2 + b.s * a.e20,
-            .e12 = b.e12 * a.s + b.e2 * a.e1 - b.e1 * a.e2 + b.s * a.e12,
-            .e012 = b.e012 * a.s + b.e12 * a.e0 + b.e20 * a.e1 + b.e01 * a.e2 +
-                b.e2 * a.e01 + b.e1 * a.e20 + b.e0 * a.e12 + b.s * a.e012,
-        };
-    }
-
-    /// regressive product
-    pub fn join(a: MV, b: MV) MV {
-        // the reverse order of a/b results in a changed minus sign for the result
-        // i don't know if it matters though...
-        // return dual(meet(dual(b), dual(a)));
-
-        // this straight up implementation is probably faster anyway
-        return MV{
-            .s = a.s * b.e012 + a.e0 * b.e12 + a.e1 * b.e20 + a.e2 * b.e01 +
-                a.e01 * b.e2 + a.e20 * b.e1 + a.e12 * b.e0 + a.e012 * b.s,
-            .e0 = a.e0 * b.e012 + a.e01 * b.e20 - a.e20 * b.e01 + a.e012 * b.e0,
-            .e1 = a.e1 * b.e012 - a.e01 * b.e12 + a.e12 * b.e01 + a.e012 * b.e1,
-            .e2 = a.e2 * b.e012 + a.e20 * b.e12 - a.e12 * b.e20 + a.e012 * b.e2,
-            .e01 = a.e01 * b.e012 + a.e012 * b.e01,
-            .e20 = a.e20 * b.e012 + a.e012 * b.e20,
-            .e12 = a.e12 * b.e012 + a.e012 * b.e12,
-            .e012 = a.e012 * b.e012,
-        };
-    }
-
-    /// inner product
-    pub fn dot(a: MV, b: MV) MV {
-        return MV{
-            .s = b.s * a.s + b.e1 * a.e1 + b.e2 * a.e2 - b.e12 * a.e12,
-            .e0 = b.e0 * a.s + b.s * a.e0 - b.e01 * a.e1 + b.e20 * a.e2 +
-                b.e1 * a.e01 - b.e2 * a.e20 - b.e012 * a.e12 - b.e12 * a.e012,
-            .e1 = b.e1 * a.s + b.s * a.e1 - b.e12 * a.e2 + b.e2 * a.e12,
-            .e2 = b.e2 * a.s + b.e12 * a.e1 + b.s * a.e2 - b.e1 * a.e12,
-            .e01 = b.e01 * a.s + b.e012 * a.e2 + b.s * a.e01 + b.e2 * a.e012,
-            .e20 = b.e20 * a.s + b.e012 * a.e1 + b.s * a.e20 + b.e1 * a.e012,
-            .e12 = b.e12 * a.s + b.s * a.e12,
-            .e012 = b.e012 * a.s + b.s * a.e012,
-        };
-    }
-
-    pub fn smul(s: f32, mv: MV) MV {
-        return MV{
-            .s = s * mv.s,
-            .e0 = s * mv.e0,
-            .e1 = s * mv.e1,
-            .e2 = s * mv.e2,
-            .e01 = s * mv.e01,
-            .e20 = s * mv.e20,
-            .e12 = s * mv.e12,
-            .e012 = s * mv.e012,
-        };
-    }
-};
+}
 
 test "basic functionality" {
     std.debug.print("\n", .{});
     const ga = CL201;
-    const ga_mv = CL201MV;
 
-    // const p = ga.MV.fromCart(2, 3);
-    // std.debug.print("{}\n", .{p});
-    // std.debug.print("{}\n", .{ga.dual(p)});
-    // std.debug.print("{}\n", .{ga.dual(ga.dual(p))});
-
-    // std.debug.print("{}\n", .{ga.meet(
-    //     ga.MV.fromEq(1, 1, 0),
-    //     ga.MV.fromEq(-1, 1, 0),
-    // )});
-    // std.debug.print("{}\n", .{ga.meet(
-    //     ga.MV.fromEq(-1, 1, 0),
-    //     ga.MV.fromEq(1, 1, 0),
-    // )});
-    // std.debug.print("{}\n", .{ga.meet(
-    //     ga.MV.fromEq(-1, -1, 2),
-    //     ga.MV.fromEq(1, -1, 0),
-    // )});
-    // std.debug.print("{}\n", .{ga.meet(
-    //     ga.MV.fromEq(-1, -1, 2),
-    //     ga.MV.fromEq(1, -1, 0),
-    // )});
-    std.debug.print("{}\n", .{ga_mv.MV.fromCart(-1, -1)});
     std.debug.print("{}\n", .{ga.Point.fromCart(-1, -1)});
-    std.debug.print("{}\n", .{ga_mv.MV.fromCart(1, 1)});
     std.debug.print("{}\n", .{ga.Point.fromCart(-1, -1)});
-    std.debug.print("join {}\n", .{ga_mv.join(
-        ga_mv.MV.fromCart(-1, -1),
-        ga_mv.MV.fromCart(1, 1),
-    )});
     std.debug.print("join {}\n", .{ga.join(
         ga.Point.fromCart(-1, -1),
         ga.Point.fromCart(1, 1),
     )});
-    std.debug.print("{}\n", .{ga_mv.MV.fromCart(-1, -1)});
     std.debug.print("{}\n", .{ga.normalized(ga.Point.fromCart(-1, -1))});
-    std.debug.print("meet {}\n", .{ga_mv.meet(
-        ga_mv.MV.fromEq(-1, -1, 2),
-        ga_mv.MV.fromEq(1, -1, 0),
-    )});
-    std.debug.print("meet {}\n", .{ga.meet(
-        ga.Line.fromEq(-1, -1, 2),
-        ga.Line.fromEq(1, -1, 0),
-    )});
 
     std.debug.print("{}\n", .{ga.apply(
         ga.Motor{ .s = 1, .e01 = 1, .e20 = 1, .e12 = 1 },
@@ -587,26 +443,6 @@ test "basic functionality" {
         ga.Point.fromCart(1, 2),
         ga.Line.fromEq(1, 1, 0),
     )});
-
-    // var mv = ga_mv.MV{ .e01 = 1, .e20 = 1 }; // a direction
-    // std.debug.print("{}\n", .{mv});
-    // mv = ga_mv.normalize(mv);
-    // std.debug.print("{}\n", .{mv});
-
-    // std.debug.print("{}\n", .{ga_mv.normalize(ga_mv.mul(
-    //     ga_mv.dot(
-    //         ga_mv.MV.fromEq(1, -1, 0),
-    //         ga_mv.MV.fromCart(2, 0),
-    //     ),
-    //     ga_mv.MV.fromEq(1, -1, 0),
-    // ))}); // projection of point onto line
-    // std.debug.print("{}\n", .{ga_mv.normalize(ga_mv.mul(
-    //     ga_mv.mul(
-    //         ga_mv.MV.fromEq(1, -1, 0),
-    //         ga_mv.MV.fromCart(2, 0),
-    //     ),
-    //     ga_mv.MV.fromEq(1, -1, 0),
-    // ))}); // reflection of point across line
 
     try std.testing.expect(true);
 }
