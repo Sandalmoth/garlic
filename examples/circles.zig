3ce12dbe0465d540637765168d0c89fd478116a3
Jonathan <sandalmoth@pm.me>
2023-06-21 16:31:53 +0200

    add example stub

---
+++ examples/circles.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+const garlic = @import("../src/garlic");
+
+const ga = garlic.CL201;
+
+pub fn main() void {
+    // clear the screen so we can use the terminal for rendering
+    // std.debug.print("\x1B[2J\x1B[H", .{});
+}
